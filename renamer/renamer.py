193a1813b796269cad5a05a12162815cb29e5c3d
Kevin Tyrrell <kev070892@gmail.com>
2022-02-17 21:53:02 -0800

    Primary sprint done

--- renamer/renamer.py
+++ renamer/renamer.py
@@ -19,42 +19,52 @@
 from argparse import ArgumentParser
 
 import decorators as de
-from directory import Directory, FileMetadata
-
-"""
-TODO: Renamer operations:
-* -t tvmode: incorporates seasons into the formatting
-* -c confirm: performs the rename on the files in the directory
-
-TODO: Done
-* -f flatten: removes holes in the files so all are consecutive
-* -z zeroes: enables or disables leading zeroes on files
-* -e extension: changes the extension of the output files
-"""
+from directory import ConcreteDirectory, FileMetadata
 
 
 def main():
-    ap = ArgumentParser(description="CLI tool written in Python 3 used to systemically rename file "
-                                    "in a directory while adhering to a variety of criteria")
+    args = ArgumentParser(description="CLI tool written in Python 3 used to systemically rename file "
+                                      "in a directory while adhering to a variety of criteria")
     # Required arguments
-    ap.add_argument("path", type=str, help="Absolute or relative path to the directory")
+    args.add_argument("path", type=str, help="Absolute or relative path to the directory")
     # Optional arguments
-    ap.add_argument("-s", "--shift", dest="shift", type=int,
-                    help="Shifts all numerical values in filenames by a specified (neg/pos) offset")
-    ap.add_argument("-f", "--format", dest="format", type=str,
-                    help="Output format of the filename, containing '%d' format specifier for the numerical pattern")
-    ap.add_argument("-c", "--consecutive", dest="consecutive", action="store_true",
-                    help="Modifies numerical values in filenames such that the values are consecutive")
-    ap.add_argument("-z", "--zeroes", dest="zeroes", type=int,
-                    help="Number of maximum leading zeroes to format numerical values (0 for automatic)")
-    ap.add_argument("-e", "--ext", dest="ext", type=str,
-                    help="Replaces the extension of files in the directory with a specified extension")
-    ap.add_argument("-m", "--mute", dest="mute", action="store_false",
-                    help="Squelches the console output of filenames and their renamed filename")
-    ap.add_argument("-y", "--yes", dest="confirm", action="store_true",
-                    help="Confirms the operation and makes changes to your filesystem according to the parameters")
-    args = ap.parse_args()
+    args.add_argument("-s", "--shift", dest="shift", type=int,
+                      help="Shifts all numerical values in filenames by a specified (neg/pos) offset")
+    args.add_argument("-f", "--format", dest="format", type=str,
+                      help="Output format of the filename, containing '%d' format specifier for the numerical pattern")
+    args.add_argument("-c", "--consecutive", dest="consecutive", action="store_true",
+                      help="Modifies numerical values in filenames such that the values are consecutive")
+    args.add_argument("-z", "--zeroes", dest="zeroes", type=int,
+                      help="Number of maximum leading zeroes to format numerical values (0 for automatic)")
+    args.add_argument("-e", "--ext", dest="ext", type=str,
+                      help="Replaces the extension of files in the directory with a specified extension")
+    args.add_argument("-m", "--mute", dest="mute", action="store_false",
+                      help="Squelches the console output of filenames and their renamed filename")
+    args.add_argument("-y", "--yes", dest="confirm", action="store_true",
+                      help="Confirms the operation and makes changes to your filesystem according to the parameters")
+    args = args.parse_args()
 
+    # Process arguments
+    directory = ConcreteDirectory(args.path)
+    dec = de.NumeratedDecorator(directory)
+    if args.shift:
+        dec = de.ShifterDecorator(dec, args.shift)
+    if args.format:
+        dec = de.FormatDecorator(dec, args.format)
+    if args.consecutive:
+        dec = de.FlattenDecorator(dec)
+    if args.zeroes:
+        dec = de.ZeroesDecorator(dec, args.zeroes)
+    if args.ext:
+        dec = de.ExtensionDecorator(dec, args.ext)
+    dec.operate()  # Perform operations according to decorators
 
+    if args.mute:
+        for old, new in directory.get_files():
+            print("Renaming [{}] --> [{}]".format(old, str(new)))
+    if args.confirm:
+        directory.save_files()
+
+        
 if __name__ == '__main__':
     main()
