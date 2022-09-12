f96041f187f709b220b87807091347299b6fb4ae
Kevin Tyrrell <kev070892@gmail.com>
2022-09-12 08:57:30 -0700

    Revamp directed graph

--- renamer/util/directedgraph.py
+++ renamer/util/directedgraph.py
@@ -17,8 +17,7 @@
 """
 
 from __future__ import annotations
-from typing import TypeVar, Dict, Tuple, Callable, Deque, List
-from collections import deque
+from typing import TypeVar
 
 from util.util import require_non_none
 
@@ -26,64 +25,44 @@ T = TypeVar("T")
 
 
 class DAG:
-    def __init__(self, vertexes_edges: Dict[T, Tuple[T, ...]], ignore_unknown_vertices: bool=True):
-        # Construct all vertices
-        data_map = {v: DAG.__DAGNode(v) for v in vertexes_edges.keys()}
-        # Setup all edges
-        for v, t in vertexes_edges.items():
-            v = data_map[v]
-            for e in require_non_none(t):
-                # Ignore edges to nodes outside of the vertex set
-                if require_non_none(e) in data_map:
-                    e = data_map[e]
-                    v.edges.add(e)
-                    e.degree += 1
-                elif not require_non_none(ignore_unknown_vertices):
-                    raise ValueError("DAG found edge not present in the vertex set: " + str(e))
-        self.__vertices = set(data_map.values())
+    def __init__(self):
+        """
+        Constructs a directed acyclic graph instance
+        """
+        self.__nodes = {}  # Map[data] -> Node
 
-    def topological_sort(self, cycle_break_cb: Callable[[T, T], None] = None):
-        output = []
-        if len(self.__vertices) > 0:
-            if cycle_break_cb is None:  # No breaker provided, cycles are not tolerated
-                cycle_break_cb = DAG.__cyclic_no_tolerance
-            order = deque(filter(lambda x: x.degree == 0, self.__vertices))
-            while True:
-                if len(order) > 0:
-                    self.__eval_acyclic(order, output)
-                if len(output) >= len(self.__vertices):
-                    break
-                # Cycles are present -- Handle appropriately
-                v = next(iter(self.__vertices))
-                for e in v.edges:
-                    e.degree -= 1
-                    if e.degree <= 0:
-                        order.append(e)
-                    cycle_break_cb(v.data, e.data)
-        return output
+    def add_vertex(self, data: T) -> bool:
+        """
+        :param data: Data of the vertex
+        :return: False if the graph already contains the vertex
+        """
+        if require_non_none(data) in self.__nodes:
+            return False
+        self.__nodes[data] = DAG.__DAGNode(data)
+        return True
 
-    # Evaluates an acyclic set of nodes, sorting them topologically.
-    def __eval_acyclic(self, order: Deque[DAG.__DAGNode], output: List[DAG.__DAGNode]) -> None:
-        while True:
-            v = order.pop()
-            for e in v.edges:
-                e.degree -= 1
-                if e.degree <= 0:
-                    order.append(e)
-            output.append(v.data)
-            self.__vertices.remove(v)
-            if len(order) <= 0:
-                return
-
-    @staticmethod
-    def __cyclic_no_tolerance(_, __) -> None:
-        raise Exception("Direct graph contains cycles and cannot be topologically sorted")
-
-    def __break_cycle(self, order: Deque[DAG.__DAGNode]) -> DAG.__DAGNode:
-        pass
+    def add_edge(self, vertex: T, edge: T) -> bool:
+        """
+        :param vertex: Vertex in which to add an edge
+        :param edge: Connected vertex from the specified vertex
+        :return: False if the vertex already contains the specified edge
+        """
+        if require_non_none(vertex) not in self.__nodes:
+            raise ValueError("Vertex is not present within the graph: {}".format(vertex))
+        if require_non_none(edge) not in self.__nodes:
+            raise ValueError("Vertex edge is not present within the graph: {}".format(edge))
+        if vertex == require_non_none(edge):
+            raise ValueError("Vertex is already a strongly connected component with itself: {}".format(vertex))
+        v, e = self.__nodes[vertex], self.__nodes[edge]
+        if e in v.edges:
+            return False
+        v.edges.add(e)
+        e.degree += 1
+        return True
 
     class __DAGNode:
         def __init__(self, data: T):
             self.data = data
             self.edges = set()
             self.degree = 0
+
